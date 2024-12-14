dca24379dac4a1f00edfce0bc926dd6487382482
IAFEnvoy <tanksheng545@126.com>
2024-12-14 13:06:32 +0800

    remove unused code

--- forge/src/main/java/com/iafenvoy/dhks/impl/forge/LoaderPlatformImpl.java
+++
@@ -1,9 +0,0 @@
-package com.iafenvoy.dhks.impl.forge;
-
-import net.minecraftforge.fml.ModList;
-
-public class LoaderPlatformImpl {
-    public static boolean isModLoaded(String modId) {
-        return ModList.get().isLoaded(modId);
-    }
-}
